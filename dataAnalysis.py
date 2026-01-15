9121c3840552e393045828e6ea0eae5d52ec2220
meikled8665 <meikled8665@student.faytechcc.edu>
2026-01-15 19:17:54 +0000

    Add settings for Python environment and implement data analysis script

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -1,1 +1,21 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 
+df = pd.read_csv("best_selling_albums.csv")
+
+print(df.head())
+
+year = df["Year"]
+sales = df["Sales_Millions"]
+
+oldest = df[df["Year"] == year.min()]
+newest = df[df["Year"] == year.max()]["Album"]
+highest_sales = df[df["Sales_Millions"] == sales.max()]
+lowest_sales = df[df["Sales_Millions"] == sales.min()]
+
+print("\n")
+print(oldest)
+print(newest)
+print(lowest_sales)
+print(highest_sales)
