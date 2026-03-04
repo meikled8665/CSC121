fb5dfeb20ce0a1dd85b62a5d9711c30c1be114c6
meikled8665 <meikled8665@student.faytechcc.edu>
2026-03-04 19:27:23 +0000

    hkbhbbhjbjb

--- M5Lab/Main.py
+++ M5Lab/Main.py
@@ -1,4 +1,40 @@
 import csv
 
-with open("sales.csv", "r", newline = "") as file:
-    reader = csv.reader(file)
+
+def getTotalSales():
+    
+    totalSales = []
+    productID = []
+
+    with open("sales.csv", "r", newline = "") as file:
+        
+        reader = csv.reader(file)
+        next(reader, None)
+        
+        for row in reader:
+            
+            totalSales.append(float(row[4]) * float(row[5]))
+            productID.append(row[2])
+
+    return totalSales, productID
+
+
+def saveTotalSales(totalSales, productID):
+    
+    fieldNames = ["Product ID", "Total Sales"]
+    
+    with open("total_sales.csv", "w", newline = "", ) as file:
+        
+        writer = csv.DictWriter(file, fieldnames = fieldNames)
+        writer.writeheader()
+        
+        for i in range(len(totalSales)):
+            writer.writerow({fieldNames[0] : productID[i], fieldNames[1] : str(f"${totalSales[i]:,.2f}")})
+            
+
+'''          
+def totalSalesPerCust:
+    pass
+'''
+            
+saveTotalSales(getTotalSales()[0], getTotalSales()[1])
